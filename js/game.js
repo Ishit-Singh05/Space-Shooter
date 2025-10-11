7aa91e3802e32909a3924d6e65dc17702188d2d3
Ishit-Singh05 <ishitperihar@gmail.com>
2025-10-11 21:16:03 +0530

    Functionality of Firing Bullets, Spawning Enemies and Medkits added

--- js/game.js
+++ js/game.js
@@ -17,3 +17,38 @@ document.addEventListener("mousemove", (e) => {
 document.addEventListener("touchmove", (e) => {
   mouseX = e.touches[0].clientX;
 });
+
+
+// Fire bullets automatically
+setInterval(() => {
+  const bullet = document.createElement("div");
+  bullet.classList.add("bullet");
+
+  const bulletOffsetX = 50; // adjust this value to match your PNG tip
+  bullet.style.left = player.offsetLeft + player.offsetWidth / 2 - 3 - bulletOffsetX + "px";
+
+  bullet.style.top = player.offsetTop - 10 + "px";
+  gameArea.appendChild(bullet);
+  bullets.push(bullet);
+}, 200);
+
+
+// Spawn enemies
+setInterval(() => {
+  const enemy = document.createElement("div");
+  enemy.classList.add("enemy");
+  enemy.style.left = Math.random() * (window.innerWidth - 60) + "px";
+  enemy.style.top = "-40px";
+  gameArea.appendChild(enemy);
+  enemies.push(enemy);
+}, 500);
+
+// Spawn healthkits
+setInterval(() => {
+  const kit = document.createElement("div");
+  kit.classList.add("healthkit");
+  kit.style.left = Math.random() * (window.innerWidth - 32) + "px";
+  kit.style.top = "-32px";
+  gameArea.appendChild(kit);
+  healthkits.push(kit);
+}, 5200);
